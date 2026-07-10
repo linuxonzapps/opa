92a28fd0ffed772c868b897d08935cdcca27f6ff
rposts <rishi.investigate@gmail.com>
2026-07-10 14:12:17 +0000

    Add custom GO path

--- src/build.sh
+++ src/build.sh
@@ -19,6 +19,9 @@ bash /tmp/linux-on-ibm-z-scripts/OPA/${version}/build_opa.sh -y
 tar cvfz opa-${version}-linux-s390x.tar.gz -C $PWD/opa opa_linux_s390x
 # Create container image
 export PATH=$PATH:/usr/local/go/bin
+# Ensure custom Go path is accounted for
+GO_VERSION=`awk -F'[=" ]+' '$1=="GO_VERSION"{print $2; exit}' /tmp/linux-on-ibm-z-scripts/OPA/${version}/build_opa.sh`
+export PATH=$PATH:/usr/local/go-${GO_VERSION}/bin
 cd opa/ && make image-s390x
 docker save -o opa-${version}-linux-s390x.container.tar openpolicyagent/opa
 mv opa-${version}-linux-s390x.container.tar ../ && cd ../
